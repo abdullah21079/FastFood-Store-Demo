8c63505b21b7eddc443d6087fecd9dfcad1680b3
abdullah007masud <abdullahalmasud.512512@gmail.com>
2024-05-10 16:54:35 +0600

    FastFood Demo

--- payment/models.py
+++ payment/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 class Payment(models.Model):
     name=models.CharField(max_length=50)
-    
+    epin=models.IntegerField(default=True)
 
     
